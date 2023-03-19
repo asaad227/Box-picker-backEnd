e8f6710e68f1431751b3dc0360aaad82c8cb42da
asaad227 <a.saad227@yahoo.com>
2023-03-19 20:03:51 +0000

    connection function created for mongodb and env connection string created

---
+++ db/conn.js
@@ -0,0 +1,29 @@
+const {MongoClient} = require('mongodb');
+const connectionString = process.env.ATLAS_URI
+const client = new MongoClient(connectionString,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+let dbConnection;
+export async function connectToServer(callback) {
+
+	const result = await client.connect((err, db)=>{
+        if(err || !db){
+            return callback(err)
+        }
+
+        dbConnection = db.db("game");
+
+        console.log("Successfully connected to mongoDb.");
+
+        return callback()
+
+    })
+
+    return result;
+}
+
+export async function getDb(){
+    return await dbConnection;
+}
