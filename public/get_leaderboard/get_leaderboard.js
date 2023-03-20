f4bcc1849c95596e5c97cc5c69bb230dd46ad471
asaad227 <a.saad227@yahoo.com>
2023-03-20 11:44:59 +0000

    update handler function

--- public/get_leaderboard/get_leaderboard.js
+++ public/get_leaderboard/get_leaderboard.js
@@ -5,7 +5,7 @@ const mongoClient = new MongoClient(process.env.MONGODB_URI);
 
 const clientPromise = mongoClient.connect();
 
-const handler = async (event) => {
+const handler = async () => {
     try {
         const database = (await clientPromise).db(process.env.MONGODB_DATABASE);
         const collection = database.collection(process.env.MONGODB_COLLECTION);
